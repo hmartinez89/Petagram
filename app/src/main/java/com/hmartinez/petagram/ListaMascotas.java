c1ce3486d9b2498131c57f81eb8e805f4b6ab18e
hmartinez89 <hmartinezale@live.com.mx>
2017-09-03 23:01:53 -0500

    Listo: Tarea semana 3, módulo 3.

--- app/src/main/java/com/hmartinez/petagram/ListaMascotas.java
+++ app/src/main/java/com/hmartinez/petagram/ListaMascotas.java
@@ -1,15 +1,20 @@
 package com.hmartinez.petagram;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
-public class ListaMascotas extends AppCompatActivity {
+public class ListaMascotas extends AppCompatActivity{
     ArrayList<Mascota> alMascotas;
     private RecyclerView rvListaMascotas;
     @Override
@@ -17,8 +22,9 @@ public class ListaMascotas extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_mascotas);
 
-        Toolbar tbPetagram = (Toolbar) findViewById(R.id.tbPetagram);
-        setSupportActionBar(tbPetagram);
+        Toolbar abListaMascotas = (Toolbar)findViewById(R.id.abListaMascotas);
+        setSupportActionBar(abListaMascotas);
+        getSupportActionBar().setLogo(R.drawable.icons8_cat_footprint_filled_50);
 
         rvListaMascotas = (RecyclerView) findViewById(R.id.rvMascotas);
 
@@ -28,8 +34,31 @@ public class ListaMascotas extends AppCompatActivity {
 
         inicializarListaMascotas();
         inicializarAdaptador();
-
     }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.listamascotas_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        switch(item.getItemId()){
+            case R.id.mMascotasFav:
+                Collections.sort(alMascotas, Mascota.likesMascotas);
+
+                Intent i = new Intent(this, MascotasFavoritas.class);
+                int n=0;
+                while(n<5 && alMascotas.get(n).getsLikes()!="0"){
+                    i.putExtra(String.format(getResources().getString(R.string.mascotasFav_param),n),alMascotas.get(n));
+                    n++;
+                }
+                i.putExtra(String.valueOf(R.string.totalMascotasFav_param), n);
+                startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     public void inicializarAdaptador(){
         MascotaAdapter adaptador = new MascotaAdapter(alMascotas);
         rvListaMascotas.setAdapter(adaptador);
@@ -38,9 +67,9 @@ public class ListaMascotas extends AppCompatActivity {
     public void inicializarListaMascotas(){
         alMascotas = new ArrayList<Mascota>();
 
-        alMascotas.add(new Mascota("Perrito", R.drawable.perro));
+        alMascotas.add(new Mascota("Perrito", "3", R.drawable.perro));
         alMascotas.add(new Mascota("Gatito", R.drawable.gato));
-        alMascotas.add(new Mascota("Conejito", R.drawable.conejo));
+        alMascotas.add(new Mascota("Conejito", "5", R.drawable.conejo));
         alMascotas.add(new Mascota("Pajarito", R.drawable.pajaro));
         alMascotas.add(new Mascota("Koala", R.drawable.koala));
     }
--- app/src/main/java/com/hmartinez/petagram/MascotasFavoritas.java
+++ app/src/main/java/com/hmartinez/petagram/MascotasFavoritas.java
@@ -2,6 +2,8 @@ package com.hmartinez.petagram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 public class MascotasFavoritas extends AppCompatActivity {
 
@@ -10,6 +12,12 @@ public class MascotasFavoritas extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mascotas_favoritas);
 
+        Toolbar abMascotasFav = (Toolbar) findViewById(R.id.abMascotasFav);
+        setSupportActionBar(abMascotasFav);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Bundle extras = this.getIntent().getExtras();
+        Mascota[] aMascotasFav = new Mascota[(int) extras.get(String.valueOf(R.string.totalMascotasFav_param))];
+
     }
 }
